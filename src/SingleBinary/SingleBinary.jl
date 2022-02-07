ad6ea6a2bd02250a690ccd49b541d0df4034d2e6
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-02-07 14:28:21 -0300

    Create SingleBinary

---
+++ src/SingleBinary/SingleBinary.jl
@@ -0,0 +1,20 @@
+module SingleBinary
+
+import PSRClassesInterface
+import Dates
+
+const PSRI = PSRClassesInterface
+
+include("reader.jl")
+include("writer.jl")
+
+function PSRI.convert_file(
+    ::Type{Reader},
+    ::Type{Writer},
+    path_from::String;
+    path_to::String = "",
+)
+    error("Conversion with OpenBinary.Reader and OpenBinary.Writer is a no op.")
+end
+
+end
