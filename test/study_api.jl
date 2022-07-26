327d5a8b43bfbad150e86240aaa40345c2c96f55
pedromxavier <pedromxavier@poli.ufrj.br>
2022-07-26 14:04:16 -0300

    Fix test

--- test/study_api.jl
+++ test/study_api.jl
@@ -5,13 +5,15 @@ function test_api(data_path::String)
     mkpath(temp_path)
 
     src_data = PSRI.initialize_study(PSRI.OpenInterface(); data_path=data_path)
-    raw_data = PSRI._raw(data)::Dict{String,<:Any}
+    raw_data = PSRI._raw(src_data)::Dict{String,<:Any}
+
     open(json_path, "w") do io
         JSON.print(io, raw_data)
     end
+    
     dest_data = PSRI.initialize_study(PSRI.OpenInterface(); data_path=temp_path)
 
-    @test_broken PSRI._raw(src_data) == PSRI._raw(dest_data)
+    @test PSRI._raw(src_data) == PSRI._raw(dest_data)
 end
 
 test_api(PATH_CASE_0)
