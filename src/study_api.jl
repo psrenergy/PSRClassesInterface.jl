f3e14cfe94419bf39dd2ebcb2cfc59d277768355
pedromxavier <pedromxavier@poli.ufrj.br>
2022-07-28 18:52:35 -0300

    Add tests for `set_series!`

--- src/study_api.jl
+++ src/study_api.jl
@@ -1,4 +1,75 @@
-const PSRCLASSES_DEFAULTS = JSON.parsefile(joinpath(@__DIR__, "JSON", "psrclasses.defaults.json"))
+const PSRCLASSES_DEFAULTS = JSON.parsefile(joinpath(@__DIR__, "json_metadata", "psrclasses.defaults.json"))
+const DATE_FORMAT_1 = Dates.DateFormat(raw"yyyy-mm-dd")
+const DATE_FORMAT_2 = Dates.DateFormat(raw"dd/mm/yyyy")
+
+function list_attributes(
+    data::Data,
+    name::String,
+)
+    if !haskey(data.data_struct, name)
+        error("PSR Class '$name' is not available for this study")
+    end
+
+    class_struct = data.data_struct[name]
+
+    attrs = sort(collect(keys(class_struct)))
+
+    return attrs
+end
+
+function list_attributes(
+    data::Data,
+    name::String,
+    index::Integer,
+)
+    element = _get_element(data, name, index)
+
+    attrs = sort(collect(keys(element)))
+
+    return attrs
+end
+
+function list_indexed_attributes(
+    data::Data,
+    name::String,
+    index_attr::String,
+)
+    if !haskey(data.data_struct, name)
+        error("PSR Class '$name' is not available for this study")
+    end
+
+    class_struct = data.data_struct[name]
+
+    attrs = [
+        attr for (attr, attr_data) in class_struct
+        if attr_data.index == index_attr || attr == index_attr
+    ] |> sort
+
+    return attrs
+end
+
+function list_indexed_attributes(
+    data::Data,
+    name::String,
+    index::Integer,
+    index_attr::String,
+)
+    if !haskey(data.data_struct, name)
+        error("PSR Class '$name' is not available for this study")
+    end
+
+    class_struct = data.data_struct[name]
+
+    element = _get_element(data, name, index)
+
+    attrs = [
+        attr for (attr, attr_data) in class_struct
+        if haskey(element, attr) &&
+        (attr_data.index == index_attr || attr == index_attr)
+    ] |> sort
+
+    return attrs
+end
 
 function create_element!(
     data::Data,
@@ -38,7 +109,6 @@ function _get_element(
     name::String, # classe
     index::Integer,
 )
-
     # ~ Retrieves raw JSON-like dict, i.e. `Dict{String, Any}`.
     # ~ `_raw(data)` is a safe interface for `data.raw`.
     # ~ This dictionary was created by reading a JSON file.
@@ -60,13 +130,11 @@ function _get_element(
     return element
 end
 
-function set_parm!(
+function _get_attr_data(
     data::Data,
     name::String,
-    index::Integer,
     attr::String,
-    value::T,
-) where {T<:MainTypes}
+)
     if !haskey(data.data_struct, name)
         error("PSR Class '$name' is not available for this study")
     end
@@ -79,16 +147,53 @@ function set_parm!(
 
     attr_data = class_struct[attr]::Attribute
 
+    return attr_data
+end
+
+@doc raw"""
+"""
+function _parse_parm end
+
+function _parse_parm(::X, ::T) where {X,T<:MainTypes}
+    error("Invalid type '$X' for parsing as '$T'")
+end
+
+_parse_parm(::Nothing, ::Type{T}) where {T<:MainTypes} = nothing
+_parse_parm(value::T, ::Type{T}) where {T<:MainTypes} = value
+_parse_parm(value::Int64, ::Type{Int32}) = convert(Int32, value)
+
+function _parse_parm(date::String, ::Type{Dates.Date})
+    for date_format in [DATE_FORMAT_1, DATE_FORMAT_2]
+        value = tryparse(Dates.Date, date, date_format)
+
+        if !isnothing(value)
+            return value
+        end
+    end
+
+    error("Invalid date format '$date'")
+end
+
+function _parse_vector(vector::Vector, ::Type{T}) where {T<:MainTypes}
+    return [_parse_parm(entry, T) for entry in vector]
+end
+
+function get_parm(
+    data::Data,
+    name::String,
+    index::Integer,
+    attr::String,
+)
+    attr_data = _get_attr_data(data, name, attr)
+
     if attr_data.is_vector
         error(
             """
             Attribute '$attr' PSR Class '$name' is a vector, not a scalar parameter.
-            Consider using `PSRI.set_vector!` instead
+            Consider using `PSRI.get_vector` instead
             """
         )
     end
-
-    attr_type = attr_data.type
 
     # ~ This is assumed to be a mutable dictionary.
     element = _get_element(data, name, index)
@@ -103,29 +208,52 @@ function set_parm!(
         error("Invalid attribute '$attr' for object of type '$name'")
     end
 
-    element[attr] = value::attr_type
-
-    nothing
+    return _parse_parm(element[attr], attr_data.type)
 end
 
-function get_vector(
+function set_parm!(
     data::Data,
     name::String,
     index::Integer,
     attr::String,
-    type::Union{Type{<:T},Nothing}=nothing,
+    value::T,
 ) where {T<:MainTypes}
-    if !haskey(data.data_struct, name)
-        error("PSR Class '$name' is not available for this study")
+    attr_data = _get_attr_data(data, name, attr)
+
+    if attr_data.is_vector
+        error(
+            """
+            Attribute '$attr' PSR Class '$name' is a vector, not a scalar parameter.
+            Consider using `PSRI.set_vector!` instead
+            """
+        )
     end
 
-    class_struct = data.data_struct[name]
+    # ~ This is assumed to be a mutable dictionary.
+    element = _get_element(data, name, index)
 
-    if !haskey(class_struct, attr)
-        error("PSR Class '$name' has no attribute '$attr'")
+    # ~ In fact, all attributes must be set beforehand.
+    # ~ Schema validation would be useful here, since there would be no need
+    #   to check for existing keys and `get_element` could handle all necessary
+    #   consistency-related work.
+    # ~ This could even be done at loading time or if something is modified by
+    #   methods like `set_parm!`.
+    if !haskey(element, attr)
+        error("Invalid attribute '$attr' for object of type '$name'")
     end
 
-    attr_data = class_struct[attr]::Attribute
+    element[attr] = _parse_parm(value, attr_data.type)
+
+    nothing
+end
+
+function _get_vector_ref(
+    data::Data,
+    name::String,
+    index::Integer,
+    attr::String,
+)
+    attr_data = _get_attr_data(data, name, attr)
 
     if !attr_data.is_vector
         error(
@@ -142,12 +270,24 @@ function get_vector(
         error("Invalid attribute '$attr' for object of type '$name'")
     end
 
-    vector = element[attr]
+    return element[attr]::Vector
+end
+
+function get_vector(
+    data::Data,
+    name::String,
+    index::Integer,
+    attr::String,
+    type::Union{Type{<:T},Nothing}=nothing,
+) where {T<:MainTypes}
+    vector = _get_vector_ref(data, name, index, attr)
+
+    attr_data = _get_attr_data(data, name, attr)
 
     if isnothing(type)
-        return Vector(vector)
+        return _parse_vector(vector, attr_data.type)
     else
-        return Vector{type}(vector)
+        return _parse_vector(vector, type)
     end
 end
 
@@ -156,26 +296,97 @@ function set_vector!(
     name::String,
     index::Integer,
     attr::String,
-    vector::Vector{T}
+    buffer::Vector{T}
 ) where {T<:MainTypes}
-    buffer = get_vector(data, name, index, attr)
+    vector = _get_vector_ref(data, name, index, attr)
 
-    if length(vector) != length(buffer)
+    if length(buffer) != length(vector)
         error(
             """
-            Vector length change from $(length(buffer)) to $(length(vector)) is not allowed.
+            Vector length change from $(length(vector)) to $(length(buffer)) is not allowed.
             Use `PSRI.set_series!` instead.
             """
         )
     end
 
-    # ~ Validation on `name` & `attr` already happened during `get_vector`
-    attr_data = data.data_struct[name][attr]
-    attr_type = attr_data.type
+    # ~ Validation on `name` & `attr` already happened during `_get_vector_ref`
+    attr_data = _get_attr_data(data, name, attr)
 
     # ~ Modify data in-place
-    for i = eachindex(buffer)
-        buffer[i] = vector[i]::attr_type
+    for i = eachindex(vector)
+        vector[i] = _parse_parm(buffer[i], attr_data.type)
+    end
+
+    nothing
+end
+
+function get_series(
+    data::Data,
+    name::String,
+    index::Integer,
+    index_attr::String,
+)
+    series = Dict{String,Vector}()
+
+    for attr in list_indexed_attributes(data, name, index, index_attr)
+        series[attr] = get_vector(data, name, index, attr)
+    end
+
+    return series
+end
+
+function set_series!(
+    data::Data,
+    name::String,
+    index::Integer,
+    index_attr::String,
+    buffer::Dict{String,Vector}
+)
+    series = get_series(data, name, index, index_attr)
+
+    valid = true
+
+    if length(buffer) != length(series)
+        valid = false
+    end
+
+    for attr in keys(series)
+        if !haskey(buffer, attr)
+            valid = false
+            break
+        end
+    end
+
+    if !valid
+        missing_attrs = setdiff(keys(series), keys(buffer))
+        for attr in missing_attrs
+            @error "Missing attribute '$(attr)'"
+        end
+
+        invalid_attrs = setdiff(keys(buffer), keys(series))
+        for attr in invalid_attrs
+            @warn "Invalid attribute '$(attr)'"
+        end
+
+        error("Invalid attributes for series indexed by $(index_attr)")
+    end
+
+    new_length = nothing
+
+    for vector in values(buffer)
+        if isnothing(new_length)
+            new_length = length(vector)
+        end
+
+        if length(vector) != new_length
+            error("All vectors must be of the same length in a series")
+        end
+    end
+
+    element = _get_element(data, name, index)
+
+    for (attr, vector) in buffer
+        element[attr] = vector
     end
 
     nothing
--- test/study_api.jl
+++ test/study_api.jl
@@ -10,10 +10,87 @@ function test_api(data_path::String)
     open(json_path, "w") do io
         JSON.print(io, raw_data)
     end
-    
+
     dest_data = PSRI.initialize_study(PSRI.OpenInterface(); data_path=temp_path)
 
-    @test PSRI._raw(src_data) == PSRI._raw(dest_data)
+    @test PSRI._raw(dest_data) == raw_data
+
+    # ~ set_parm!
+    parm_data = Dict(
+        "PSRThermalPlant" => [
+            "ComT" => Int32(33),
+        ]
+        # ~ TODO: Add more test cases later, as in:
+        #   "PSRClass" => ["attr" => value...]
+    )
+
+    for (name, parm_list) in parm_data
+        for (attr, new_value) in parm_list
+            old_value = PSRI.get_parm(src_data, name, 1, attr)
+            @test old_value != new_value
+            PSRI.set_parm!(src_data, name, 1, attr, new_value)
+            value_set = PSRI.get_parm(src_data, name, 1, attr)
+            @test new_value == value_set
+        end
+    end
+
+    # ~ set_vector!
+    vector_data = Dict(
+        "PSRThermalPlant" => [
+            "Data" => Dates.Date.(["1900-01-02"]),
+        ]
+        # ~ TODO: Add more test cases later, as in:
+        #   "PSRClass" => ["attr" => [value...]...]
+    )
+
+    for (name, vector_list) in vector_data
+        for (attr, new_value) in vector_list
+            old_value = PSRI.get_vector(src_data, name, 1, attr)
+            @test old_value != new_value
+            PSRI.set_vector!(src_data, name, 1, attr, new_value)
+            value_set = PSRI.get_vector(src_data, name, 1, attr)
+            @test new_value == value_set
+        end
+    end
+
+    # ~ set_series!
+    series_data = Dict(
+        "PSRThermalPlant" => [
+            "Data" => Dict{String, Vector}(    
+                "GerMin"   => [0.0, 1.0],
+                "GerMax"   => [888.0, 777.0],
+                "O&MCost"  => [0.0, 1.0],
+                "NGas"     => [nothing, nothing],
+                "IH"       => [0.0, 0.0],
+                "ICP"      => [0.0, 0.0],
+                "Data"     => Dates.Date.(["1900-01-01", "1900-01-02"]),
+                "CoefE"    => [1.0, 2.0],
+                "CTransp"  => [0.0, 1.0],
+                "PotInst"  => [888.0, 777.0],
+                "Existing" => [0, 0],
+                "sfal"     => [0, 1],
+                "NAdF"     => [0, 0],
+                "Unidades" => [1, 1],
+                "StartUp"  => [0.0, 2.0],
+            )
+        ]
+        # ~ TODO: Add more test cases later, as in:
+        #   "PSRClass" => [
+        #       "index_attr" => Dict{String, Vector}(
+        #           "attr" => [value...]...
+        #       )...
+        #   ]
+    )
+
+    for (name, series_list) in series_data
+        for (attr, new_value) in series_list
+            old_value = PSRI.get_series(src_data, name, 1, attr)
+            @test old_value != new_value
+            PSRI.set_series!(src_data, name, 1, attr, new_value)
+            value_set = PSRI.get_series(src_data, name, 1, attr)
+            @test new_value == value_set
+        end
+    end
 end
 
 test_api(PATH_CASE_0)
