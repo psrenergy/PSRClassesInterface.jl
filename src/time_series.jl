c1b3c1fcc370a7a4db5beac18802fe6bacd48232
pedroripper <pedros.ripper@gmail.com>
2023-01-17 13:57:19 -0300

    Refactoring

--- src/time_series.jl
+++ src/time_series.jl
@@ -1,5 +1,5 @@
 # Check if study has a time series for a collection stored in a Graf file
-function has_graf_file(data::Data, collection::String)
+function has_graf_file(data::Data, collection::String, attribute::String)
     _check_collection_in_study(data, collection)
 
     if !haskey(data.raw, collection)   
@@ -11,7 +11,7 @@ function has_graf_file(data::Data, collection::String)
     end
 
     for graf in data.raw["GrafScenarios"]
-        if graf["classname"] == collection
+        if graf["classname"] == collection && graf["vector"] == attribute
             return true
         end
     end
@@ -20,7 +20,11 @@ function has_graf_file(data::Data, collection::String)
 end
 
 # Get time series stored in a graf file for a collection
-function get_time_series(data::Data, collection::String)
+function get_series(
+    data::Data, 
+    collection::String, 
+    attribute::String
+    )
     if !has_graf_file(data, collection)
         error("No time series file for collection '$collection'")
     end
@@ -28,7 +32,7 @@ function get_time_series(data::Data, collection::String)
     graf_files = Vector{String}()
 
     for graf in data.raw["GrafScenarios"]
-        if graf["classname"] == collection
+        if graf["classname"] == collection && graf["vector"] == attribute 
             append!(graf_files, graf["binary"])
         end
     end
@@ -44,3 +48,83 @@ function get_time_series(data::Data, collection::String)
 
     return time_series_data
 end
+
+function set_series!(
+    data::Data, 
+    collection::String, 
+    attribute::String, 
+    file_name::String = "grafscenarios"
+    )
+    
+
+    
+end
+
+# function set_series!(
+#     data::Data,
+#     path::String,
+#     data::Array{Float64,4};
+#     collection::String,
+#     agent_attribute::String,
+#     unit::String = "",
+#     initial_stage::Int32,
+#     initial_year::Int32,
+# )
+#     attributes = _get_indexed_attributes(data, collection, index, indexing_attribute)
+
+#     valid = true
+
+#     if length(buffer) != length(attributes)
+#         valid = false
+#     end
+
+#     for attribute in attributes
+#         if !haskey(buffer, attribute)
+#             valid = false
+#             break
+#         end
+#     end
+
+#     if !valid
+#         missing_attrs = setdiff(attributes, keys(buffer))
+
+#         for attribute in missing_attrs
+#             @error "Missing attribute '$(attribute)'"
+#         end
+
+#         invalid_attrs = setdiff(keys(buffer), attributes)
+
+#         for attribute in invalid_attrs
+#             @error "Invalid attribute '$(attribute)'"
+#         end
+
+#         error("Invalid attributes for series indexed by $(indexing_attribute)")
+#     end
+
+#     new_length = nothing
+
+#     for vector in values(buffer)
+#         if isnothing(new_length)
+#             new_length = length(vector)
+#         end
+
+#         if length(vector) != new_length
+#             error("All vectors must be of the same length in a series")
+#         end
+#     end
+
+#     element = _get_element(data, collection, index)
+
+#     # validate types
+#     for (attribute, vector) in buffer
+#         attribute_struct = get_attribute_struct(data, collection, attribute)
+#         _check_type(attribute_struct, eltype(vector), collection, attribute)
+#     end
+
+#     for (attribute, vector) in buffer
+#         # protect user's data
+#         element[attribute] = deepcopy(vector)
+#     end
+
+#     return nothing
+# end
